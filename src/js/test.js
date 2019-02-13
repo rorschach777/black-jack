4b2f08ccdab573796976a84b608f3d1748c851e5
rorschach777 <msweitzer1987@gmail.com>
2019-02-13 11:44:30 -0500

    Added Styling

--- src/js/test.js
+++ src/js/test.js
@@ -1,7 +1,6 @@
 let btn = document.querySelector('#new-game-button');
 let btn2 = document.querySelector('#hit-button');
-let titleFront = document.querySelector('.black-jack-title-front');
-let titleBack = document.querySelector('.black-jack-title-back');
+
 
 btn.addEventListener('click', ()=>{
     console.log('hi')
